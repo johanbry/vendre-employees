bd21db68d439047307d25b6bc93e78cbfcc790ce
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-07-09 14:21:46 +0200

    add styling

--- src/views/EmployeesView.vue
+++ src/views/EmployeesView.vue
@@ -52,7 +52,9 @@ watch(
 
 <template>
   <main>
-    <section class="employees-list">
+    <h1>Medarbetare</h1>
+    <p>Vår största tillgång hos Vendre är våra fantastiska medarbetare.</p>
+    <section class="employees-list full-width">
       <EmployeeCard
         v-for="employee in employeesList"
         :key="employee.id"
@@ -63,8 +65,63 @@ watch(
       />
     </section>
 
-    <button v-for="n in totalPages" :key="n" @click="changePage(n)">
-      Sida {{ n }}
-    </button>
+    <section class="pagination">
+      <p>Visar sida {{ currentPage }} av {{ totalPages }}</p>
+      <button v-for="n in totalPages" :key="n" @click="changePage(n)">
+        Sida {{ n }}
+      </button>
+    </section>
   </main>
 </template>
+
+<style scoped>
+main {
+  text-align: center;
+}
+.employees-list {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  list-style-type: none;
+  gap: 3rem;
+  background-image: linear-gradient(-45deg, #45a5ec, #5333ed, #45a5ec);
+  padding: 3rem 3rem;
+  margin: 3rem 0 2rem 0;
+}
+
+.full-width {
+  width: 100vw;
+  position: relative;
+  left: 50%;
+  right: 50%;
+  margin-left: -50vw;
+  margin-right: -50vw;
+}
+
+.pagination {
+  font-size: 14px;
+}
+
+button {
+  border: 2px solid #5333ed;
+  border-radius: 4px;
+  background-color: #fff;
+  padding: 0.5rem 1rem;
+  margin: 0.5rem;
+  cursor: pointer;
+  text-transform: uppercase;
+  font-weight: bold;
+  transition: background-color 0.5s, color 0.5s;
+}
+
+button:hover {
+  background-color: #5333ed;
+  color: white;
+}
+
+@media (min-width: 640px) {
+  .employees-list {
+    padding: 5rem 5rem;
+  }
+}
+</style>
