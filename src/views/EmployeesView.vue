c5fe6c1cf789eb145afce0e526e494f4c1fbad9b
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-07-09 10:54:25 +0200

    add error handling

--- src/views/EmployeesView.vue
+++ src/views/EmployeesView.vue
@@ -9,7 +9,7 @@ type TEmployee = {
   avatar: string;
 };
 
-const API_URL = `https://reqres.in/api/users`;
+const API_URL = `https://reqres.in/api/users/fr`;
 let isLoading = false;
 let totalPages: number;
 const employeesList = ref<TEmployee[]>([]);
@@ -17,13 +17,14 @@ const employeesList = ref<TEmployee[]>([]);
 const fetchData = async () => {
   try {
     isLoading = true;
-    const res = await fetch(`${API_URL}}`);
+    const res = await fetch(`${API_URL}`);
+    if (!res.ok) throw new Error(res.status.toString());
     const data = await res.json();
     employeesList.value = data.data;
     totalPages = data.total_pages;
     isLoading = false;
   } catch (error) {
-    console.log(error);
+    console.log("Error fetching data: " + error);
   }
 };
 fetchData();
