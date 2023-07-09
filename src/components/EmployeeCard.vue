467bea615f73daa32686ce9ff580811bb97059c8
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-07-09 11:24:45 +0200

    add component to show employee card

---
+++ src/components/EmployeeCard.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+interface IProps {
+  first_name: string;
+  last_name: string;
+  email: string;
+  avatar: string;
+}
+defineProps<IProps>();
+</script>
+
+<template>
+  {{ first_name }}
+  {{ last_name }}
+  {{ email }}
+  {{ avatar }}
+</template>
+<style scoped></style>
