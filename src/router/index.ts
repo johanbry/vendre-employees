94973f6c896aee0251f5f05e65d67225d0f9ca00
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-07-09 17:30:22 +0200

    change path

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/employees",
+      path: "employees",
       name: "employees",
       component: EmployeesView,
     },
